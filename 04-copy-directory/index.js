465ba7b1a71ccdaa0da57d675a9c21bee1dd0406
Maxim <melnichenkomaxim97@gmail.com>
2023-05-08 21:40:22 +0300

    feat: started task in 04-copy-directory but not finish

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, error => {
+    if (error) throw error;
+});
+
+fs.copyFile()
