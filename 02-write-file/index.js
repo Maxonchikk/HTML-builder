6bab1541af60cfb3c40c1114db006fc793730df4
Maxim <melnichenkomaxim97@gmail.com>
2023-05-08 09:57:23 +0300

    feat: made writting words in the text file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,26 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const writableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
-// const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 const { stdin, stdout } = process;
-
-const EventEmmiter = require('events');
-const exit = new EventEmmiter();
-
-exit.on('exit', (exit) => {
-    console.log('Удачи!');
-    process.exit();
-});
-
-emitter.emit('exit');
 
 stdout.write('Введите текст...\n')
 stdin.on('data', data => {
+    if (data.toString().trim() === 'exit') {
+        console.log('Ввод завершен. Спасибо!');
+        process.exit()
+    }
     const str = data.toString();
     writableStream.write(str);
-    process.exit();
-
 })
 
-process.on('exit', () => stdout.write('Удачи'));
+process.on('SIGINT', () => {
+    console.log('Ввод завершен');
+    process.exit()
+})
+
 process.on('error', error => stdout('Error', error.message));
